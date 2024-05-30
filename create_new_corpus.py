29f8d3d24c81d258a30fce8bbef17f76161fb1e0
ErwannE <erwannesteve@gmail.com>
2024-05-30 18:25:50 +0200

    mise a niveau

---
+++ create_new_corpus.py
@@ -0,0 +1,20 @@
+#J'utilise ce document pour créer de nouveau corpus, l'original étai trop long pour être utilisé dans le code.
+file_path = 'c:/Users/erwan/Documents/ENPC/2A/PRAMA/PRAMA_Projet/europarl-v6.fr-en.fr' 
+
+try:
+    file = open(file_path, 'r', encoding='utf-8')
+except IOError:
+    print("Erreur lors de l'ouverture du fichier.")
+text = file.read()
+
+n = 1000000
+new_file_path = f'c:/Users/erwan/Documents/ENPC/2A/PRAMA/PRAMA_Projet/toy_corpus/new_corpus_{n}.txt'
+
+try:
+    with open(new_file_path, 'w', encoding='utf-8') as new_file:
+        new_file.write(text[:n])
+except IOError:
+    print("Erreur lors de la création du nouveau fichier.")
+
+
+print(f'New corpus created at {new_file_path}')
